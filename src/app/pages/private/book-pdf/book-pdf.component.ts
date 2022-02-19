c729200b045a49b9f159ba2a1ac536070ff227a6
Mariana <mariana.velasquez0502@gmail.com>
2022-02-19 15:58:43 -0500

    FUNCIONÓ HPTA

--- src/app/pages/private/book-pdf/book-pdf.component.ts
+++ src/app/pages/private/book-pdf/book-pdf.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MessageI } from 'src/app/models/MessageI';
 import { ChatService } from 'src/app/services/chat.service';
+import { FormControl } from "@angular/forms";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book-pdf',
@@ -11,14 +13,22 @@ import { ChatService } from 'src/app/services/chat.service';
 
 export class BookPDFComponent implements OnInit {
   
-  @Input() msgs: Array<MessageI> =[{"user":"../../../../../../assets/img/NoImage.png","content":"Holaaaa","time":"8:50"},{"user":"../../../../../../assets/img/NoImage.png","content":"Adiossss","time":"8:50"},{"user":"../../../../../../assets/img/NoImage.png","content":"Holaaaa","time":"8:50"}];
-  msg: string;
+  msgs: Array<MessageI> =[{"user":"../../../../../../assets/img/NoImage.png","content":"Holaaaa","time":"8:50"},{"user":"../../../../../../assets/img/NoImage.png","content":"Adiossss","time":"8:50"},{"user":"../../../../../../assets/img/NoImage.png","content":"Holaaaa","time":"8:50"}];
+  msgForm: FormControl;
+  subscriptionList: {
+    connection: Subscription,
+    newMgs: Subscription
+  } = {
+      connection: undefined,
+      newMgs: undefined
+  };
 
   constructor(public chatService:ChatService) {
-    
+    this.msgForm = new FormControl();
    }
 
   ngOnInit(): void { 
+    this.initChat();
   }
 
   showFiller = false;
@@ -39,14 +49,24 @@ export class BookPDFComponent implements OnInit {
   onProgress(progressData) {
     console.log('(progress)', progressData); // do anything with progress data. For example progress indicator
   }
+
+  initChat() {
+    this.subscriptionList.connection = this.chatService.connect().subscribe(_ => {
+      console.log("Nos conectamos");
+      this.subscriptionList.newMgs = this.chatService.getNewMsgs().subscribe((msg: MessageI) => {
+        this.msgs.push(msg);
+      });
+    });
+  }
+
   sendMsg() {
     const msg: MessageI = {
-      content: this.msg,
-      time: "8:58",
-      user: ""
+      content: this.msgForm.value,
+      time: Date.now().toString(),
+      user: "../../../../../../assets/img/NoImage.png"
     }
     this.chatService.sendMsg(msg);
-    this.msg = "";
+    this.msgForm.setValue("");
   }
 }
 
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,7 +1,7 @@
-import * as io from 'socket.io-client';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MessageI } from '../models/MessageI';
+import { io } from "socket.io-client";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +15,10 @@ export class ChatService {
 
   connect() {
     return new Observable(observer => {
-      this.socket = io('localhost:3000');
+      // this.socket = io('http://192.168.0.23:3000');
+      this.socket = io('http://localhost:3000',{ transports: ['websocket'] });
       this.socket.on('connect', () => {
+        this.socket.emit("connected");
         observer.next();
       })
     })
@@ -31,6 +33,7 @@ export class ChatService {
   }
 
   sendMsg(msg: MessageI) {
+    console.log(msg)
     this.socket.emit('newMsg', msg);
   }
 
